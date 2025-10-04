efa6127803dba8d8c067b9daf9ae1ea9a4b1fada
Leo Du <wpsy20xinjund8@gmail.com>
2025-10-04 15:57:24 -0700

    Settings Screen
    Added setting screen

--- Assets/menuScript.cs
+++ Assets/menuScript.cs
@@ -1,14 +1,33 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.Audio;
+using UnityEngine.UI;
 public class menuScript : MonoBehaviour 
 {
+    [SerializeField] GameObject settingScreen;
+    [SerializeField] AudioMixer mixer;
     public void enterGame()
     {
         SceneManager.LoadScene("Game");
     }
+
+    public void openSettings()
+    {
+        settingScreen.SetActive(true);
+    }
+
     public void exitGame()
     {
         Application.Quit();
+    }
+
+    public void exitSetting()
+    {
+        settingScreen.SetActive(false);
+    }
+
+    public void setVolume(float volume)
+    {
+        mixer.SetFloat("volume", volume);
     }
 }
